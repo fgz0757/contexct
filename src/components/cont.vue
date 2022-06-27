9b0948779e729b1146f27752b1d85e81754c5a2b
DESKTOP-FKCENQT\冯广志 <FGZ0814@163.com>
2022-06-27 16:48:17 +0800

    ss

---
+++ src/components/cont.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <slot name="header"></slot>
+    </div>
+    <div>
+      <slot name="body"></slot>
+    </div>
+    <div>
+      <slot name="footer" :txt="text"></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Cont",
+  data() {
+    return {
+      text: "这是底部",
+    };
+  },
+};
+</script>
